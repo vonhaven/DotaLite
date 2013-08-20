32111d1e3a223318210d24b3eae87a58637bc899
vonhaven <vonhaven@vonhaven.(none)>
2013-08-20 11:11:35 -0400

    Bootstrap WR, VS, Lina

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -61,6 +61,99 @@ class BootStrap {
             e: Spell.findByName("Spell Shield"),
             r: Spell.findByName("Mana Void")
         ]).save(failOnError: true, flush: true)
+
+        new Spell(
+            name: "Magic Missile",
+            description: "Deals 20 magic damage to the enemy, stunning it for 1.5 seconds.",
+            cooldown: 8,
+            manaCost: 100
+        ).save(failOnError: true, flush: true)
+        new Spell(
+            name: "Wave of Terror",
+            description: "Deals 4 magic damage to enemies in a line, reducing their armor by 3 for 4 seconds.",
+            cooldown: 4,
+            manaCost: 20
+        ).save(failOnError: true, flush: true)
+        new Spell(
+            name: "Vengeance Aura",
+            description: "Increases base attack damage of all allied heroes within 250 range by 30%.",
+            passive: true
+        ).save(failOnError: true, flush: true)
+        new Spell(
+            name: "Nether Swap",
+            description: "Swaps location with the target hero.",
+            cooldown: 12,
+            manaCost: 75
+        ).save(failOnError: true, flush: true)
+        new Hero ([
+            name: "Vengeful Spirit",
+            flavor: "Halp John.",
+            bio: "Halp John",
+            faction: faction,
+            primaryStat: primaryStat,
+            baseHealth: 125,
+            baseMana: 105,
+            baseDamage: 8,
+            baseArmor: 3,
+            baseStrength: 18,
+            baseAgility: 22,
+            baseIntelligence: 18,
+            baseAttackSpeed: 5,
+            baseMovementSpeed: 29,  
+            q: Spell.findByName("Magic Missile"),
+            w: Spell.findByName("Wave of Terror"),
+            e: Spell.findByName("Vengeance Aura"),
+            r: Spell.findByName("Nether Swap")
+        ]).save(failOnError: true, flush: true)
+        
+        //Radiant Intelligence
+        primaryStat = Stat.INT
+        new Spell(
+            name: "Powershot",
+            description: "Deal up to 20 magic damage to all enemies in a line, diminishing 20% per target hit.",
+            cooldown: 6,
+            manaCost: 50
+        ).save(failOnError: true, flush: true)
+        new Spell(
+            name: "Shackleshot",
+            description: "Attempt to latch the enemy to another enemy directly behind it, stunning both enemies for 2 seconds. Otherwise, stun the initial target for 0.5 seconds.",
+            cooldown: 6,
+            manaCost: 50
+        ).save(failOnError: true, flush: true)
+        new Spell(
+            name: "Windrun",
+            description: "Run at maximum movement speed and evades all attacks for 3 seconds.",
+            cooldown: 10,
+            manaCost: 50
+        ).save(failOnError: true, flush: true)
+        new Spell(
+            name: "Focus Fire",
+            description: "Attack at maximum speed, dealing 50% damage on each attack. Lasts up to 10 seconds.",
+            cooldown: 20,
+            manaCost: 75
+        ).save(failOnError: true, flush: true)
+        new Hero ([
+            name: "Windrunner",
+            flavor: "Pew.",
+            bio: "Moer like winrun hue.",
+            faction: faction,
+            primaryStat: primaryStat,
+            baseHealth: 125,
+            baseMana: 105,
+            baseDamage: 8,
+            baseArmor: 3,
+            baseStrength: 18,
+            baseAgility: 22,
+            baseIntelligence: 18,
+            baseAttackSpeed: 5,
+            baseMovementSpeed: 29,  
+            q: Spell.findByName("Shackleshot"),
+            w: Spell.findByName("Powershot"),
+            e: Spell.findByName("Windrun"),
+            r: Spell.findByName("Focus Fire")
+        ]).save(failOnError: true, flush: true)
+
+        //Dire Strength
         
         //Radiant Intelligence
         primaryStat = Stat.INT
--- grails-app/views/heroes/index.gsp
+++ grails-app/views/heroes/index.gsp
@@ -15,7 +15,6 @@
                 <g:img class="spell" dir="images" file="spells/icons/${it.w.name.replace(' ', '_')}.png"/>
                 <g:img class="spell" dir="images" file="spells/icons/${it.e.name.replace(' ', '_')}.png"/>
                 <g:img class="spell" dir="images" file="spells/icons/${it.r.name.replace(' ', '_')}.png"/>
-                <div class="flavor">${it.flavor}</div>
             </div>
         </g:each>
     </body>
